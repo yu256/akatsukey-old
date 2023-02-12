d236f0a177340cf584f175f6e5ef3a54f504f4b1
tamaina <tamaina@hotmail.co.jp>
2023-02-12 15:48:56 +0000

    perf(sw): skipWaitingしない

--- packages/sw/src/sw.ts
+++ packages/sw/src/sw.ts
@@ -8,7 +8,7 @@ import * as swos from '@/scripts/operations';
 import { acct as getAcct } from '@/filters/user';
 
 self.addEventListener('install', ev => {
-	ev.waitUntil(self.skipWaiting());
+	//ev.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', ev => {
