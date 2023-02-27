28c761d2765206f4c3cacbd500ab587e69f26b75
yu1024 <yu256g@gmail.com>
2023-02-28 00:11:29 +0900

    Revert "akatsukey-settingsを改修"
    This reverts commit a2b45d83137f9db95a397696df52f0c4b9fbda0b.

--- packages/client/src/pages/settings/akatsukey-settings.vue
+++ packages/client/src/pages/settings/akatsukey-settings.vue
@@ -1,97 +1,26 @@
 <template>
-<div class="_formRoot">
-	<MkInfo warn class="_formBlock">設定は自動で保存されません。画面下部の保存ボタンを使用してください。</MkInfo>
+	<div class="_formRoot">
+		<FormInfo class="_formBlock">以下の機能はすべてベータ版です。</FormInfo>
 
-	<FormSection>
-		<FormSwitch v-model="navBarChatIcon" class="_formBlock">
-			チャットを表示
-			<template #caption>モバイルのボトムナビゲーションにチャットを表示します。</template>
-		</FormSwitch>
-
-		<FormSwitch v-model="navBarReloadIcon" class="_formBlock">
-			リロードを表示
-			<template #caption>モバイルのボトムナビゲーションにリロードを表示します。</template>
-		</FormSwitch>
-
-		<FormSwitch v-model="navBarWidgetIcon" class="_formBlock">
-			ウィジェットを表示
-			<template #caption>モバイルのボトムナビゲーションにウィジェットを表示します。</template>
-		</FormSwitch>
-
-		<FormSwitch v-model="postFormEmojiPickerNewStyleEnabled" class="_formBlock">
-			新しい絵文字ピッカーを使用する
-			<template #caption>投稿画面の下に絵文字ピッカーを表示します。</template>
-		</FormSwitch>
-	
-		<FormSwitch v-model="postFormFooterEmojiIconEnabled" class="_formBlock">
-			絵文字ボタンを表示
-			<template #caption>投稿画面のフッターに絵文字ボタンを表示します。</template>
-		</FormSwitch>
-	</FormSection>
-
-	<FormSection>
-		<FormSwitch v-model="RenoteCollapsedEnabled" class="_formBlock">
-			一度見たノートのRenoteを畳む
-			<template #caption>タイムライン上の一度見たノートのRenoteを畳んで表示します。本文をクリックすると展開します。</template>
-		</FormSwitch>
-	</FormSection>
-
-	<MkButton class="_formBlock" primary :disabled="!changed" @click="save"><i class="ti ti-device-floppy"></i> {{ i18n.ts.save }}</MkButton>
-</div>
+		<FormSwitch v-model="navBarChatIcon" class="_formBlock">チャットを表示する</FormSwitch>
+		<FormSwitch v-model="navBarReloadIcon" class="_formBlock">リロードを表示する</FormSwitch>
+		<FormSwitch v-model="navBarWidgetIcon" class="_formBlock">ウィジェットを表示する</FormSwitch>
+		<FormSwitch v-model="postFormEmojiPickerNewStyleEnabled" class="_formBlock">投稿画面で新しい絵文字ピッカーを使用する</FormSwitch>
+		<FormSwitch v-model="postFormFooterEmojiIconEnabled" class="_formBlock">投稿画面のフッターに絵文字ボタンを表示する</FormSwitch>
+		<FormSwitch v-model="RenoteCollapsedEnabled" class="_formBlock">タイムライン上の一度見たノートのRenoteを畳んで表示します。本文をクリックすると展開します。</FormSwitch>
+	</div>
 </template>
-
+	
 <script lang="ts" setup>
-import { watch } from 'vue';
-import FormSwitch from '@/components/form/switch.vue';
-import FormSection from '@/components/form/section.vue';
-import MkButton from '@/components/MkButton.vue';
-import MkInfo from '@/components/MkInfo.vue';
-import * as os from '@/os';
-import { unisonReload } from '@/scripts/unison-reload';
-import { i18n } from '@/i18n';
+import { computed, ref, watch } from 'vue';
 import { defaultStore } from '@/store';
+	import FormInfo from '@/components/MkInfo.vue';
+	import FormSwitch from '@/components/form/switch.vue';
 
-let changed = $ref(false);
-
-const navBarChatIcon = $ref(defaultStore.state.navBarChatIcon);
-const navBarReloadIcon = $ref(defaultStore.state.navBarReloadIcon);
-const navBarWidgetIcon = $ref(defaultStore.state.navBarWidgetIcon);
-const postFormEmojiPickerNewStyleEnabled = $ref(defaultStore.state.postFormEmojiPickerNewStyleEnabled);
-const postFormFooterEmojiIconEnabled = $ref(defaultStore.state.postFormFooterEmojiIconEnabled);
-const RenoteCollapsedEnabled = $ref(defaultStore.state.RenoteCollapsedEnabled);
-
-watch([
-	$$(navBarChatIcon),
-	$$(navBarReloadIcon),
-	$$(navBarWidgetIcon),
-	$$(postFormEmojiPickerNewStyleEnabled),
-	$$(postFormFooterEmojiIconEnabled),
-	$$(RenoteCollapsedEnabled),
-], () => {
-	changed = true;
-});
-
-async function save(): Promise<void> {
-	if (await check()) {
-		defaultStore.set('navBarChatIcon', navBarChatIcon);
-		defaultStore.set('navBarReloadIcon', navBarReloadIcon);
-		defaultStore.set('navBarWidgetIcon', navBarWidgetIcon);
-		defaultStore.set('postFormEmojiPickerNewStyleEnabled', postFormEmojiPickerNewStyleEnabled);
-		defaultStore.set('postFormFooterEmojiIconEnabled', postFormFooterEmojiIconEnabled);
-		defaultStore.set('RenoteCollapsedEnabled', RenoteCollapsedEnabled);
-
-		const { canceled } = await os.confirm({
-			type: 'info',
-			text: i18n.ts.reloadToApplySetting,
-		});
-
-		if (canceled) return;
-
-		unisonReload();
-	} else {
-		os.alert({
-			type: 'error',
-		});
-	}
-}
+	const navBarChatIcon = computed(defaultStore.makeGetterSetter('navBarChatIcon'));
+	const navBarReloadIcon = computed(defaultStore.makeGetterSetter('navBarReloadIcon'));
+	const navBarWidgetIcon = computed(defaultStore.makeGetterSetter('navBarWidgetIcon'));
+	const postFormEmojiPickerNewStyleEnabled = computed(defaultStore.makeGetterSetter('postFormEmojiPickerNewStyleEnabled'));
+	const postFormFooterEmojiIconEnabled = computed(defaultStore.makeGetterSetter('postFormFooterEmojiIconEnabled'));
+	const RenoteCollapsedEnabled = computed(defaultStore.makeGetterSetter('defaultStore.state.RenoteCollapsedEnabled'));
 </script>
