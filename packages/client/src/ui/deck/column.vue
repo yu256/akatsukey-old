e210140690b01fc2d3d106a117b3a35426e3f53f
taiyme <53635909+taiyme@users.noreply.github.com>
2023-03-10 06:01:24 +0900

    enhance(frontend): デッキUIのカラムにリロードボタンを追加
    - ウィンドウのリロードボタンみたいな感じ

--- packages/client/src/ui/deck/column.vue
+++ packages/client/src/ui/deck/column.vue
@@ -23,9 +23,10 @@
 			<slot name="action"></slot>
 		</div>
 		<span class="header"><slot name="header"></slot></span>
+		<button v-tooltip="i18n.ts.reload" class="reload _button" @click.stop="reload"><i class="ti ti-reload"></i></button>
 		<button v-tooltip="i18n.ts.settings" class="menu _button" @click.stop="showSettingsMenu"><i class="ti ti-dots"></i></button>
 	</header>
-	<div v-show="active" ref="body">
+	<div v-show="active" :key="reloadCount" ref="body">
 		<slot></slot>
 	</div>
 </section>
@@ -67,6 +68,8 @@ watch($$(dragging), v => os.deckGlobalEvents.emit(v ? 'column.dragStart' : 'colu
 let draghover = $ref(false);
 let dropready = $ref(false);
 
+let reloadCount = $ref(0);
+
 const isMainColumn = $computed(() => props.column.type === 'main');
 const active = $computed(() => props.column.active !== false);
 watch($$(active), v => emit('change-active-state', v));
@@ -87,6 +90,10 @@ onBeforeUnmount(() => {
 	os.deckGlobalEvents.off('column.dragStart', onOtherDragStart);
 	os.deckGlobalEvents.off('column.dragEnd', onOtherDragEnd);
 });
+
+const reload = (): void => {
+	reloadCount++;
+};
 
 function onOtherDragStart() {
 	dropready = true;
@@ -352,6 +359,7 @@ function onDrop(ev) {
 
 		> .toggleActive,
 		> .action > ::v-deep(*),
+		> .reload,
 		> .menu {
 			z-index: 1;
 			width: var(--deckColumnHeaderHeight);
@@ -377,6 +385,11 @@ function onDrop(ev) {
 
 		> .action:empty {
 			display: none;
+		}
+
+		> .reload {
+			margin-left: auto;
+			margin-right: 0px;
 		}
 
 		> .menu {
