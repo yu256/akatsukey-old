03ce4bc9a38530f974a1625f184c113c5a1c4752
taiyme <53635909+taiyme@users.noreply.github.com>
2023-03-11 00:03:16 +0900

    fix(frontend): ポップアップメニューのクリック判定が貫通する

--- packages/client/src/components/MkMenu.vue
+++ packages/client/src/components/MkMenu.vue
@@ -33,7 +33,7 @@
 			<span v-else-if="item.type === 'switch'" :tabindex="i" class="item" @mouseenter.passive="onItemMouseEnter(item)" @mouseleave.passive="onItemMouseLeave(item)">
 				<FormSwitch v-model="item.ref" :disabled="item.disabled" class="form-switch">{{ item.text }}</FormSwitch>
 			</span>
-			<button v-else-if="item.type === 'parent'" :tabindex="i" class="_button item parent" :class="{ childShowing: childShowingItem === item }" @mouseenter="showChildren(item, $event)">
+			<button v-else-if="item.type === 'parent'" :tabindex="i" class="_button item parent" :class="{ childShowing: childShowingItem === item }" @click="onParentClicked(item, $event)" @mouseenter.passive="onParentMouseEnter(item, $event)">
 				<i v-if="item.icon" class="ti-fw" :class="item.icon"></i>
 				<span>{{ item.text }}</span>
 				<span class="caret"><i class="ti ti-chevron-right ti-fw"></i></span>
@@ -62,6 +62,7 @@ import FormSwitch from '@/components/form/switch.vue';
 import { MenuItem, InnerMenuItem, MenuPending, MenuAction } from '@/types/menu';
 import * as os from '@/os';
 import { i18n } from '@/i18n';
+import { isTouchUsing } from '@/scripts/touch';
 
 const XChild = defineAsyncComponent(() => import('./MkMenu.child.vue'));
 
@@ -168,6 +169,14 @@ async function showChildren(item: MenuItem, ev: MouseEvent) {
 		childShowingItem = item;
 	}
 }
+
+const onParentClicked = (item: MenuItem, ev: MouseEvent): void => {
+	if (isTouchUsing) showChildren(item, ev);
+};
+
+const onParentMouseEnter = (item: MenuItem, ev: MouseEvent): void => {
+	if (!isTouchUsing) showChildren(item, ev);
+};
 
 function clicked(fn: MenuAction, ev: MouseEvent) {
 	fn(ev);
