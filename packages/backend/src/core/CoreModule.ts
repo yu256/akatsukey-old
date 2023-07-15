2829eaa49c3d143817b639cf1027505f1f31b039
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-15 09:57:58 +0900

    enhance: 招待機能の改善 * refactor(backend): 招待機能を改修
    * feat(backend): 招待コードのcreate/delete/listエンドポイントを追加
    
    * add(misskey-js): エンドポイントと型を追加
    
    * change(backend): metaでinvite関連の情報も返すように
    
    * add(misskey-js): エンドポイントと型を追加
    
    * add(backend): `/endpoints/invite/limit`を追加
    
    * fix: createdByがnullableではなかったのを修正
    
    * fix: relationが取得できていなかった問題を修正
    
    * fix: パラメータを間違えていたのを修正
    
    * feat(client): 招待ページを実装
    
    * change(client): インスタンスメニューの「招待」押した場合に招待ページに飛ぶように変更
    
    * feat: 招待コードをコピーできるように
    
    * change(backend): metaに招待コード発行に関する情報を持たせるのをやめる
    
    * feat: ロールごとに招待コードの発行上限数などを設定できるように
    
    * change(client): 招待コードをコピーしたときにダイアログを出すように
    
    * add: 招待に関する管理者用のエンドポイントを追加
    
    * change(backend): モデレーターであれば作成者以外でも招待コードを削除できるように
    
    * change(backend): admin/invite/listはオフセットでページネーションするように
    
    * feat(client): 招待コードの管理ページを追加
    
    * feat(client): 招待コードのリストをソートできるように
    
    * change: `admin/invite/create`のレスポンスを修正
    
    * fix(client): 有効期限を指定できていなかった問題を修正
    
    * refactor: 必要のない箇所を削除
    
    * perf(backend): use limit() instead of take()
    
    * change(client): 作成ボタンを見た目を変更
    
    * refactor: 招待コードの生成部分を共通化し、コード内に"01OI"のいずれかの文字を含まないように
    
    * fix(client): paginationの仕様が変わっていたので修正
    
    * change(backend): expiresAtパラメータのnullを許容
    
    * change(client): 有効期限を設けないときは日付の入力欄を非表示に
    
    * fix: 自身のポリシーよりもインスタンス側のポリシーが優先表示される問題を修正
    
    * fix: n時間のときに「n時間間」となってしまうのを修正
    
    * fix(backend): ポリシーが途中で変更されたときに作成可能数がマイナス表記になってしまうのを修正
    
    * change(client): 招待コードのユーザー名が不明な理由を表示するように
    
    * update: CHANGELOG.md
    
    * lint
    
    * refactor
    
    * refactor
    
    * tweak ui
    
    * 🎨
    
    * 🎨
    
    * add(backend): indexを追加
    
    * change(backend): indexの追加に伴う変更
    
    * change(client): インスタンスメニューの「招待」の場所を変更
    
    * add(frontend): MkInviteCode用のstorybookを追加
    
    * Update misskey-js.api.md
    
    * fix(misskey-js): InviteのcreatedByの型が間違っていたのを修正
    
    ---------
    
    Co-authored-by: syuilo <Syuilotan@yahoo.co.jp>
    Co-authored-by: tamaina <tamaina@hotmail.co.jp>

--- packages/backend/src/core/CoreModule.ts
+++ packages/backend/src/core/CoreModule.ts
@@ -80,6 +80,7 @@ import { GalleryLikeEntityService } from './entities/GalleryLikeEntityService.js
 import { GalleryPostEntityService } from './entities/GalleryPostEntityService.js';
 import { HashtagEntityService } from './entities/HashtagEntityService.js';
 import { InstanceEntityService } from './entities/InstanceEntityService.js';
+import { InviteCodeEntityService } from './entities/InviteCodeEntityService.js';
 import { ModerationLogEntityService } from './entities/ModerationLogEntityService.js';
 import { MutingEntityService } from './entities/MutingEntityService.js';
 import { RenoteMutingEntityService } from './entities/RenoteMutingEntityService.js';
@@ -203,6 +204,7 @@ const $GalleryLikeEntityService: Provider = { provide: 'GalleryLikeEntityService
 const $GalleryPostEntityService: Provider = { provide: 'GalleryPostEntityService', useExisting: GalleryPostEntityService };
 const $HashtagEntityService: Provider = { provide: 'HashtagEntityService', useExisting: HashtagEntityService };
 const $InstanceEntityService: Provider = { provide: 'InstanceEntityService', useExisting: InstanceEntityService };
+const $InviteCodeEntityService: Provider = { provide: 'InviteCodeEntityService', useExisting: InviteCodeEntityService };
 const $ModerationLogEntityService: Provider = { provide: 'ModerationLogEntityService', useExisting: ModerationLogEntityService };
 const $MutingEntityService: Provider = { provide: 'MutingEntityService', useExisting: MutingEntityService };
 const $RenoteMutingEntityService: Provider = { provide: 'RenoteMutingEntityService', useExisting: RenoteMutingEntityService };
@@ -326,6 +328,7 @@ const $ApQuestionService: Provider = { provide: 'ApQuestionService', useExisting
 		GalleryPostEntityService,
 		HashtagEntityService,
 		InstanceEntityService,
+		InviteCodeEntityService,
 		ModerationLogEntityService,
 		MutingEntityService,
 		RenoteMutingEntityService,
@@ -444,6 +447,7 @@ const $ApQuestionService: Provider = { provide: 'ApQuestionService', useExisting
 		$GalleryPostEntityService,
 		$HashtagEntityService,
 		$InstanceEntityService,
+		$InviteCodeEntityService,
 		$ModerationLogEntityService,
 		$MutingEntityService,
 		$RenoteMutingEntityService,
@@ -562,6 +566,7 @@ const $ApQuestionService: Provider = { provide: 'ApQuestionService', useExisting
 		GalleryPostEntityService,
 		HashtagEntityService,
 		InstanceEntityService,
+		InviteCodeEntityService,
 		ModerationLogEntityService,
 		MutingEntityService,
 		RenoteMutingEntityService,
@@ -679,6 +684,7 @@ const $ApQuestionService: Provider = { provide: 'ApQuestionService', useExisting
 		$GalleryPostEntityService,
 		$HashtagEntityService,
 		$InstanceEntityService,
+		$InviteCodeEntityService,
 		$ModerationLogEntityService,
 		$MutingEntityService,
 		$RenoteMutingEntityService,
